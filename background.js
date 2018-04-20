06cebd8de8dbdf0fe82cfb22373953f1af4dc3d7
ollie314 <mehdi.lefebvre@gmail.com>
2018-04-20 15:57:58 +0200

    Initial commit

---
+++ background.js
@@ -0,0 +1,36 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is green.");
+    chrome.windows.getAll({populate:true}, winData => {
+        var tabs = [];
+        for (var i in winData) {
+          if (winData[i].focused === true) {
+              var winTabs = winData[i].tabs;
+              var totTabs = winTabs.length;
+              for (var j=0; j<totTabs;j++) {
+                tabs.push(winTabs[j].url);
+              }
+          }
+        }
+        console.log(tabs);
+    });
+    var notification = chrome.notifications.create(
+        'fe434536-481d-434d-a4c8-46e2cbbac3bf',
+        {
+            type: 'basic',
+            iconUrl: 'assets/images/account_albums_screens_tabs-512.png',
+            title: 'Hello!',
+            message: 'You have a new mail !'
+        },
+        notificationId => {
+            console.log('notification callback has been called for notification ', notificationId);
+        }
+      );
+  });
+});
