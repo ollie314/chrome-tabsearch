bff2a88efc20bca7d6944dcb08f2366024ed80f2
ollie314 <mehdi.lefebvre@gmail.com>
2018-05-02 22:22:22 +0200

    chore: reformat code

--- assets/js/tab-search.js
+++ assets/js/tab-search.js
@@ -1,23 +1,21 @@
 ((M) => {
-    document.addEventListener("DOMContentLoaded", () => {
-	const domElement = document.querySelector('.autocomplete');
-	let autoComplete; // M.Autocomplete(domElement, options);
-	chrome.tabs.query({}, tabs => {
-	    const data = {};
-	    tabs.forEach(t => {
-		data[t.title] = {
-		    value: t.title,
-		    tabId: t.id,
-		    windowId: t.windowId,
-		    status: t.status,
-		    icon: t.favIconUrl
-		}
-	    });
-	    const options = {
-		data: data
-	    };
-	    M.Autocomplete.init(domElement, options)
-	    console.log(tabs);
-	});
-    });
+  document.addEventListener("DOMContentLoaded", () => {
+	  const domElement = document.querySelector('.autocomplete');
+	  let autoComplete; // M.Autocomplete(domElement, options);
+	  chrome.tabs.query({}, tabs => {
+      const data = {};
+      tabs.forEach(t => {
+		    data[t.title] = {
+          value: t.title,
+          tabId: t.id,
+          windowId: t.windowId,
+          status: t.status,
+          icon: t.favIconUrl
+		    }
+      });
+      const options = {data};
+      M.Autocomplete.init(domElement, options)
+      console.log(tabs);
+	  });
+  });
 })(M);
